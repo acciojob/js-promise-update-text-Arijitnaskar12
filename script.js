e0cb0f4dcdcd2a141edc282c73dde5fd17e7d5d5
Arijitnaskar12 <117929311+Arijitnaskar12@users.noreply.github.com>
2023-05-26 16:05:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+let DivElement=document.getElementById("output");
+let prom=new Promise((resolve)=>{
+	setTimeOut(()=>{
+		DivElement.innerText="Hello,world";
+	},1000);
+});
